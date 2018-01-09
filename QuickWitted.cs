165f3c90134b80ebc3cde921f1badf86be95001e
Felipe Demétrio de Souza <felipeds@gmail.com>
2018-01-09 15:30:35 -0500

    Frist Commit

---
+++ QuickWitted.cs
@@ -0,0 +1,16 @@
+﻿using Harmony;
+
+namespace QuickWitted
+{
+    [HarmonyPatch(typeof(SkillsManager))]
+    [HarmonyPatch("IncrementPointsAndNotify")]
+    public class QuickWitted
+    {
+        static bool Prefix(ref int numPoints)
+        {
+            numPoints = numPoints * 2;
+            return true;
+        }
+    }
+}
+
