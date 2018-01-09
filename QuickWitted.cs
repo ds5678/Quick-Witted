6b1472a99ccb544ab70766353f48443ef7a32e16
Felipe Demétrio de Souza <felipeds@gmail.com>
2018-01-09 17:11:59 -0500

    Added XML support for better customization

--- QuickWitted.cs
+++ QuickWitted.cs
@@ -1,15 +1,76 @@
-﻿using Harmony;
+﻿using System;
+using System.Xml;
+using Harmony;
 
 namespace QuickWitted
 {
-    [HarmonyPatch(typeof(SkillsManager))]
-    [HarmonyPatch("IncrementPointsAndNotify")]
+    [HarmonyPatch(typeof(SkillsManager), "IncrementPointsAndNotify")]
     public class QuickWitted
     {
-        static bool Prefix(ref int numPoints)
+
+        static int[] learnRates;
+        static bool isRunning;
+        static XmlDocument config;
+
+        //Load ratio fo each skill;
+        public static void OnLoad()
         {
-            numPoints = numPoints * 2;
-            return true;
+
+            //Load XML and initiate the array
+            LoadXML();
+
+            int numberOfSkills = Enum.GetNames(typeof(SkillType)).Length;
+            learnRates = new int[numberOfSkills];
+
+            foreach (SkillType type in Enum.GetValues(typeof(SkillType)))
+            {
+                learnRates[(int)type] = GetRateForSkill(type);
+            }
+
+        }
+
+        static int GetRateForSkill(SkillType type)
+        {
+            try
+            {
+
+                XmlNodeList elemList = config.GetElementsByTagName(type.ToString());
+                int rv = int.Parse(elemList[0].InnerText);
+
+                return rv;
+            }
+            catch
+            {
+                //if you can't load it form the XML default it to 2
+                return 2;
+            }
+
+
+
+        }
+
+        static bool LoadXML()
+        {
+
+            config = new XmlDocument();
+            //stick this whole thin in atry for now. 
+            //TODO:Better error managment
+            try
+            {
+                config.Load("QuickWittedConfig.xml");
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        static void Prefix(SkillType skillType, ref int numPoints)
+        {
+            numPoints = numPoints * learnRates[(int)skillType];
         }
     }
 }
